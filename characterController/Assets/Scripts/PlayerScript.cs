dbb4bebaee5f26a984e203dcbd6e29429bfe6196
André Gomes <agomes@altyra.com>
2021-04-12 10:58:15 +0100

    le shoot

--- characterController/Assets/Scripts/PlayerScript.cs
+++ characterController/Assets/Scripts/PlayerScript.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    private Transform sensor;
+    private Transform sensor;    
+    private Transform mira;
     CharacterController cc;
     private LayerMask piso;
     Vector3 altura = Vector3.zero;
@@ -13,6 +14,7 @@ public class PlayerScript : MonoBehaviour
     {
         piso = LayerMask.GetMask("piso");
         sensor = GameObject.FindGameObjectWithTag("Sensor").transform;
+        mira = GameObject.FindGameObjectWithTag("Mira").transform;
         cc = GetComponent<CharacterController>();
         cc.Move(new Vector3(1, 0, 0));
     }
@@ -26,14 +28,45 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         noChao = Physics.CheckSphere(sensor.position, distChao, piso, QueryTriggerInteraction.Collide);
         if (noChao && altura.y < 0) altura.y = -2;
         float x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         float z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         Vector3 anda = transform.right * x + transform.forward * z;
         cc.Move(anda);
+
         if (noChao && Input.GetButtonDown("Jump")) altura.y = Mathf.Sqrt(gravidade * -2.0f * jump);
         altura.y += gravidade;
+
+
         cc.Move(altura);
+
+        if (Input.GetKeyDown(KeyCode.Mouse0)) shoot();
+    }
+
+    protected void shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(mira.position,mira.forward, out hit, Mathf.Infinity))
+        {
+            Debug.Log(hit.collider.gameObject.name);
+        }
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        float forca = 100f;
+        if (hit.gameObject.layer == LayerMask.NameToLayer("obstaculo"))
+        {
+            if (hit.moveDirection.y < 0) return;
+            Rigidbody rb = hit.gameObject.GetComponent<Rigidbody>();
+            if (rb != null || rb.isKinematic != true)
+            {
+                rb.velocity = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z) * forca;
+            }
+
+            Debug.Log("Yey :D");
+        }
     }
 }
